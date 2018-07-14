2fea4c712cebd221236c6ceae43e9ac84045a23e
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-15 00:12:35 +0800

    Update sql.py

--- function/sql.py
+++ function/sql.py
@@ -12,34 +12,14 @@ import urllib.parse as urlparse
 def main():
     _sql = Sql()
     command = "select * from user_config"
-#    uid = 'u001'
-#    name = 'twstar'
-#    config = '{"你幾歲":"5歲"}'
-#    sql_command = "select user_id, user_name, config from user_config where user_id ='%s'" % (uid)
-#    #print(sql_command)
-#    content = _sql.select(sql_command)
-#    
-#    print(content)
-#       
-#    #print(content)
-#    if len(content) == 0:
-#        #print('no user insert new one')
-#        command = "insert into user_config (user_id, user_name, config) values('%s','%s','%s')" % (uid,name,config)
-#        print(_sql.insert(command))
-#    else:
-#        command = "update user_config set user_name = '%s',config = '%s' where user_id = '%s'" % (name,config,uid)
-#        print(_sql.update(command))
-#        
-#    print(_sql.select(sql_command))
-
 
 class Sql(object):
         
          
     def __init__(self):
         
-        #url = urlparse.urlparse(os.environ['DATABASE_URL'])
-        url = urlparse.urlparse('postgres://emghycqpjwctwr:04a3e938a72371fd03f0ed0428836f805c95b08fc332e9976fa9f2f82cdc1549@ec2-54-83-33-213.compute-1.amazonaws.com:5432/dcfuue2glgrc2m')
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        #url = urlparse.urlparse('postgres://emghycqpjwctwr:04a3e938a72371fd03f0ed0428836f805c95b08fc332e9976fa9f2f82cdc1549@ec2-54-83-33-213.compute-1.amazonaws.com:5432/dcfuue2glgrc2m')
         
         self.class_name = 'Sql'
         self.database = url.path[1:]
